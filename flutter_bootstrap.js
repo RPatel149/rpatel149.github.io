5aa6fd4cdbb2a6e12386241840859bb8a775a9dd
rutvi <rutvikpatel1432@gmail.com>
2024-09-23 00:10:12 -0400

    First commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1459065377"
+    serviceWorkerVersion: "4020894106"
   }
 });
